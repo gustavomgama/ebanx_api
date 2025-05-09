41cd4fa0c45dd331edb2e8d751e03dd01a359e66
gustavomgama <gutomgama@gmail.com>
2025-05-09 16:27:41 -0300

    Enable rails caching

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -20,7 +20,7 @@ Rails.application.configure do
   if Rails.root.join("tmp/caching-dev.txt").exist?
     config.public_file_server.headers = { "cache-control" => "public, max-age=#{2.days.to_i}" }
   else
-    config.action_controller.perform_caching = false
+    config.action_controller.perform_caching = true
   end
 
   # Change to :null_store to avoid any caching.
